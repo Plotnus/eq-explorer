b3f26d552c1c2a3057590d10041d75982829a829
Plotnus <foo@bar.com>
2022-04-05 12:39:40 -0600

    added GraphBuilder

--- src/main.rs
+++ src/main.rs
@@ -5,53 +5,93 @@ use std::collections::HashMap;
 use std::rc::Rc;
 
 
-type MyGraph = Graph<(),()>;
+type MyGraph = Graph<(),EdgeValue>;
 type UpdateFn = Rc<dyn Fn(&mut HashMap<NodeIndex,f32>, HashMap<String,NodeIndex>)>;
+type Value = f32;
+type EdgeValue = f32;
 struct NodeSpecification {
     name: String,
     initial_value: f32,
     depends_on: Vec<&'static str>,
     update_fn: Option<UpdateFn>, // leaf nodes do not have an update fn for recalculating
+}
 
+struct MyGraphBuilder {
+    nodes: Vec<NodeSpecification>,
+}
+impl MyGraphBuilder {
+    fn new() -> Self {
+        MyGraphBuilder { nodes: vec![]}
+    }
+    fn add_nodes(mut self, mut nodes: Vec<NodeSpecification>) -> Self {
+        self.nodes.append(&mut nodes);
+        self
+    }
+
+    fn build(self) -> SomeGraph {
+        let mut out = SomeGraph::new();
+        // first lets create all the nodes, then we'll connect them
+        for n in &self.nodes {
+            let id = out.graph.add_node(());
+            out.names.insert(id, n.name.clone());
+            out.values.insert(id, n.initial_value);
+            out.ids.insert(n.name.clone(), id);
+            if let Some(update_fn) = &n.update_fn {
+                out.update_fns.insert(id, update_fn.clone());
+            }
+        }
+        // now lets add edges for who the node depends on
+        for n in &self.nodes {
+            for &dependency in &n.depends_on {
+                out.graph.add_edge(
+                    out.ids[dependency], 
+                    out.ids[&n.name],
+                    //(),
+                    -1., // we do this weight so we can update based on longest path to
+                );
+            }
+        }
+        // now that we have our graph, lets determine the updates need when input nodes change
+        // input_node - a node that can be updated
+        for i in 0..out.ids.len() {
+            let node_index = NodeIndex::new(i);
+            let path = petgraph::algo::bellman_ford(&out.graph, node_index).unwrap();
+            let mut max_distances = 
+                path.distances
+                    .iter()
+                    .enumerate()
+                    .filter(|(_,&v)| v < 0.) // since (-) node weight filters for 
+                    .map(|(i,&v)| (v.abs(),i)) // so we can sort by value
+                    .collect::<Vec<(f32,usize)>>();
+            // sort the index by distance
+            max_distances.sort_by(|a,b| a.partial_cmp(b).unwrap());
+            let update_path: Vec<NodeIndex> = {
+                max_distances.iter()
+                    .map(|(_,i)| NodeIndex::new(*i))
+                    .collect::<Vec<NodeIndex>>()
+            };
+            out.update_path.insert(node_index, update_path);
+        }
+        out
+    }
 }
 struct SomeGraph {
     graph: MyGraph, // used to track dependencies... is invisible outside of module
-    values: HashMap<NodeIndex, f32>,
-    names: HashMap<NodeIndex, String>,
-    ids: HashMap<String,NodeIndex>,
+    values: HashMap<NodeIndex, Value>, // stores the value of each node. Not much gained keeping it out of graph
+    names: HashMap<NodeIndex, String>, // keeping it out of graph allows us to print stuff about graph
+    ids: HashMap<String,NodeIndex>, // name -> id
     update_fns: HashMap<NodeIndex,UpdateFn>,
+    update_path: HashMap<NodeIndex,Vec<NodeIndex>>, // update order for consistence
 }
 impl SomeGraph {
     fn new() -> Self {
         Self {
             graph: MyGraph::new(),
             update_fns: HashMap::new(),
+            update_path: HashMap::new(),
             values: HashMap::new(),
             names: HashMap::new(),
             ids: HashMap::new(),
-        }
-    }
-    
-    fn add_nodes(&mut self, nodes: Vec<NodeSpecification>) {
-        // first lets create all the nodes, then we'll connect them
-        for n in &nodes {
-            let id = self.graph.add_node(());
-            self.names.insert(id, n.name.clone());
-            self.values.insert(id, n.initial_value);
-            self.ids.insert(n.name.clone(), id);
-            if let Some(update_fn) = &n.update_fn {
-                self.update_fns.insert(id, update_fn.clone());
-            }
-        }
-        // now lets add edges for who the node depends on
-        for n in nodes {
-            for &dependency in &n.depends_on {
-                self.graph.add_edge(
-                    self.ids[dependency], 
-                    self.ids[&n.name],
-                    (),
-                );
-            }
         }
     }
     
@@ -68,8 +108,7 @@ impl SomeGraph {
 
 
 fn main() {
-    let mut my_graph = SomeGraph::new();
-    let node_specs = vec![
+    let node_specifications = vec![
         NodeSpecification {
             name: "a".to_string(),
             initial_value: 13.,
@@ -114,6 +153,15 @@ fn main() {
             })),
         },
     ];
-    my_graph.add_nodes(node_specs);
+    let my_graph = {
+        MyGraphBuilder::new()
+            .add_nodes(node_specifications)
+            .build()
+    };
+    
+    my_graph.print_dependencies_for("a");
+    my_graph.print_dependencies_for("b");
+    my_graph.print_dependencies_for("c");
+    my_graph.print_dependencies_for("d");
     my_graph.print_dependencies_for("e");
 }
